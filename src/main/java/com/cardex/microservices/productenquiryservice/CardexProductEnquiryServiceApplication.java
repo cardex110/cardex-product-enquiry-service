7913ef78c2da245dba0b6666b7dcde3ed80c8a8f
Amine Baba <ababa@rbbn.com>
2021-12-04 12:36:13 -0500

    initial commit

---
+++ src/main/java/com/cardex/microservices/productenquiryservice/CardexProductEnquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.productenquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardexProductEnquiryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardexProductEnquiryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardex/microservices/productenquiryservice/CardexProductEnquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.productenquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardexProductEnquiryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
