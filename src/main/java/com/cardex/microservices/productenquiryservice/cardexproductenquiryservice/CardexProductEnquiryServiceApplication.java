dd7555d3123a1c7ac229aec3697a5280f0b3525d
Amine Baba <ababa@rbbn.com>
2021-12-05 10:31:41 -0500

    initial commit

--- src/main/java/com/cardex/microservices/productenquiryservice/cardexproductenquiryservice/CardexProductEnquiryServiceApplication.java
+++ src/main/java/com/cardex/microservices/productenquiryservice/cardexproductenquiryservice/CardexProductEnquiryServiceApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@EnableFeignClients
+
 @SpringBootApplication
+@EnableFeignClients("com.cardex.microservices.productenquiryservice.cardexproductenquiryservice")
+@EnableDiscoveryClient
 public class CardexProductEnquiryServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cardex/microservices/productenquiryservice/cardexproductenquiryservice/client/ProductStockClient.java
+++ src/main/java/com/cardex/microservices/productenquiryservice/cardexproductenquiryservice/client/ProductStockClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="cardex-product-stock-service", url="localhost:8800")
+@FeignClient(name="cardex-product-stock-service")
 //@FeignClient(name="cardex-product-stock-service")
 //@LoadBalancerClient(name = "cardex-product-stock-service",configuration=LoadBalancerConfiguration.class)
 public interface ProductStockClient {
